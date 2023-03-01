82ed0ddcbcaa8eaf2f01967cde8082f431b150ab
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-03-01 21:04:26 +0530

    created index.ejs and room.ejs to create room for board

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+//upon clicking on the button takes to the /uuid url
+
+const createBoard = ()=> {
+    location.href = '/uuid';
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require("socket.io")(server);
-const {v4: uuid} = require("uuid")
+const {v4: uuidv4} = require("uuid")
 
 
 
@@ -13,7 +13,17 @@ app.use(express.static("public"))
 app.get("/", (req, res)=> {
     res.render('index')
 })
-
+// redirects to the url created by uuid 
+//unique roomid created with uuid
+app.get('/uuid', (req, res) => {
+    res.redirect('/' + uuidv4())
+})
+//rendering to the room.ejs
+app.get('/:room', (req, res)=> {
+    res.render("room", {
+        roomId: req.params.room
+    })
+})
 server.listen(3000, () => {
     console.log('server is running')
 });
