e8941c4cfc852955151dac1729406995deded91c
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-03-03 21:12:44 +0530

    created logic for 2 players

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require("socket.io")(server);
 const {v4: uuidv4} = require("uuid")
-
+const ngrok = require('ngrok')
 
 
 app.use(express.json());
@@ -18,12 +18,41 @@ app.get("/", (req, res)=> {
 app.get('/uuid', (req, res) => {
     res.redirect('/' + uuidv4())
 })
+
+io.on('connection', socket => {
+    socket.on('join-room', roomId => {
+        room = io.sockets.adapter.rooms.get(roomId);
+        var roomSize = 0;
+        if(room){
+            roomSize = room.size;
+        }
+        if(roomSize < 2){
+            socket.join(roomId);
+            socket.broadcast.to(roomId).emit('user-connected');
+            socket.on('disconnect', ()=> {
+                socket.broadcast.to(roomId).emit('user-disconnected');
+            })
+
+            socket.on('can-play', () => {
+                socket.broadcast.to(roomId).emit('can-play')
+            })
+            socket.on('clicked', (id) => {
+                socket.broadcast.to(roomId).emit('clicked', id)
+            })
+        } else {
+            socket.emit('full-room')
+        }
+    })
+})
+
 //rendering to the room.ejs
 app.get('/:room', (req, res)=> {
     res.render("room", {
         roomId: req.params.room
     })
 })
+
+
 server.listen(3000, () => {
     console.log('server is running')
 });
